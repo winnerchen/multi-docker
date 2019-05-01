a9003f5861009d7b1484153b01f03d1e593c7915
Yiheng Chen <yiheng.chen@boral.com.au>
2019-05-01 21:54:32 +1000

    copy/paste file from resource

--- client/src/App.js
+++ client/src/App.js
@@ -12,12 +12,14 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">Fib Calculator version 2</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
-          <h3>Hi There!</h3>
-
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
         </div>
       </Router>
     );
